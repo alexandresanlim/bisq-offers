a1b4eb4ce359eb974d6c5ebfd59bf37e39c9f043
Alexandre Sanlim <alexandreslima@outlook.com>
2023-08-31 15:56:27 -0300

    add filter

--- app/filter.tsx
+++ app/filter.tsx
@@ -1,74 +1,70 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, StyleSheet, FlatList } from 'react-native';
 import Modal from 'react-native-modal';
+import { Pair } from './(tabs)';
 
-export interface FilterItem {
-    id: number;
-    name: string;
-    // Outras propriedades dos itens, se houver
-}
 
 export interface FilterBottomSheetProps {
     isVisible: boolean;
     onClose: () => void;
-    filterItems: (filter: FilterItem) => void;
-    filterList: FilterItem[];
+    filterItems: (filter: Pair) => void;
+    filterList: Pair[];
 }
 
 
 const FilterBottomSheet: React.FC<FilterBottomSheetProps> = ({ isVisible, onClose, filterItems, filterList }) => {
-    const handleFilter = (selectedItem: FilterItem) => {
-      filterItems(selectedItem);
-      onClose();
+    const handleFilter = (selectedItem: Pair) => {
+        filterItems(selectedItem);
+        onClose();
     };
-  
+
     return (
-      <Modal isVisible={isVisible} onBackdropPress={onClose} style={styles.modal}>
-        <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Select a pair</Text>
-          <FlatList
-            data={filterList}
-            keyExtractor={item => item.id.toString()}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                onPress={() => handleFilter(item)}
-                style={styles.filterItem}
-              >
-                <Text style={styles.filterItemText}>{item.name}</Text>
-              </TouchableOpacity>
-            )}
-            style={styles.listContainer}
-          />
-        </View>
-      </Modal>
+        <Modal isVisible={isVisible} onBackdropPress={onClose} style={styles.modal}>
+            <View style={styles.modalContent}>
+                <Text style={styles.modalTitle}>Select a fiat</Text>
+                <FlatList
+                    data={filterList}
+                    keyExtractor={item => item.pair.toString()}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity
+                            onPress={() => handleFilter(item)}
+                            style={styles.filterItem}
+                        >
+                            <Text style={styles.filterItemText}>{item.rsymbol}</Text>
+                        </TouchableOpacity>
+                    )}
+                    style={styles.listContainer}
+                />
+            </View>
+        </Modal>
     );
-  };
-  
-  const styles = StyleSheet.create({
+};
+
+const styles = StyleSheet.create({
     modal: {
-      justifyContent: 'flex-end',
-      margin: 0,
+        justifyContent: 'flex-end',
+        margin: 0,
     },
     modalContent: {
-      backgroundColor: 'white',
-      borderTopLeftRadius: 10,
-      borderTopRightRadius: 10,
-      padding: 20,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        padding: 20,
     },
     modalTitle: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginBottom: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
     },
     listContainer: {
-      maxHeight: 200,
+        maxHeight: 200,
     },
     filterItem: {
-      paddingVertical: 12,
+        paddingVertical: 12,
     },
     filterItemText: {
-      fontSize: 16,
+        fontSize: 16,
     },
-  });
-  
+});
+
 export default FilterBottomSheet;
